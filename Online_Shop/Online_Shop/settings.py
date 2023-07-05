8c038b99aa38a727a9aaee0d0c685e5e36635fa2
Leo <newoptpamela@gmail.com>
2023-07-05 10:46:15 +0200

    Start

--- Online_Shop/Online_Shop/settings.py
+++ Online_Shop/Online_Shop/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# , BASE_DIR будет содержать путь к корневой директории проекта,
+# на два уровня выше расположения файла, в котором определена эта
+# переменная.
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
